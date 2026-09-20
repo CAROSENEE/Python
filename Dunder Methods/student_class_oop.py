28a3a8635e00c202bdd6bbc8a09258f7953d8a88
CAROSINE <moshiur287rahman@gmail.com>
2026-09-20 23:48:27 +0600

    first dunder

--- Dunder Methods/student_class_oop.py
+++ Dunder Methods/student_class_oop.py
@@ -1,5 +1,17 @@
-def __str__(self):
-    return f"Student: {self.name}, Age: {self.age}"
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-def __repr__(self):
-    return f"Student({self.name!r}, {self.age})"
+    def __str__(self):
+        return f"Student: {self.name}, Age: {self.age}"
+
+    def __repr__(self):
+        return f"Student({self.name!r}, {self.age})"
+
+
+s1 = Student("Mahin", 22)
+
+print(s1)         # __str__ call hoy
+print(repr(s1))   # __repr__ call hoy
+print([s1])       # list er vitore __repr__ dekhay
